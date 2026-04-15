5c4e189684a41875487240e86b6db5940373f4a8
Nahida4479 <szymon.cyb13@gmail.com>
2026-04-15 23:07:07 +0200

    Repair

--- Nahida4479.py
+++ Nahida4479.py
@@ -1,7 +1,7 @@
 #Nahida4479
 import time
-import threading from pynput 
-import mouse, keyboard
+import threading
+from pynput import mouse, keyboard
 
 class Nahida4479Recorder:
     def __init__(self):
@@ -13,29 +13,75 @@ class Nahida4479Recorder:
         self.kb_controller = keyboard.Controller()
 
 
-        def add_event(self, event_type, data):
+    def add_event(self, event_type, data):
             if self.is_recording:
 
                 timestamp = time.time() - self.start_time
                 self.recorded_events.append((event_type, data, timestamp))
 
-        def start_recording(self):
-            self-recorded_events = []
+    def start_recording(self):
+            self.recorded_events = []
             self.start_time = time.time()
             self.is_recording = True
             print("Recording...")
 
-        def stop_recording(self):
+    def stop_recording(self):
             self.is_recording = False
             print("Recording finished")
 
 
-        def on_click(x, y, button, pressed):
+    def on_click(x, y, button, pressed):
             if pressed:
                 recorder.add_event('click', (x, y, button))
                 return False
             recorder.add_event('key', key)
+
+    def play_recording(self):
+        if not self-recorder_events:
+            print("You didn't record the script :(")
+            return
+        print("Play starts in 2 seconds")
+        time.sleep(2)
+
+
+        last_time = 0
+
+        for event_type, data, timestamp in self.recorded_events:
+            wait_time = timestamp - last_time
+            if wait_time > 0:
+                    time.sleep(wait_time)
+
+            last_time = timestamp
+
+            if event_type == "click":
+                    x, y, button = data 
+
+                    self.mouse_controller.position = (x, y)
+                    self.mouse_controller.click(button)
+
+            elif event_type == "key":
+                    try:
+                        self.kb_controller.press(data)
+                        self.kb_controller.release(data)
+                    except Exception as e:
+                        print("Error: {e}")
+
+            print("Script stop")
+
+    def on_press(key):
+        try:
+            if 'recorder' in globals() and recorder.is_recording:
+                if key == keyboard.Key.esc:
+                    recorder.stop_recording()
+                    return False
+            recorder.add_event('key', key)
+        except NameError:
+            pass
+        
         recorder = Nahida4479Recorder()
 
-    mouse_listener = mouse.Lisstener(on_click=on_click)
+    mouse_listener = mouse.Listener(on_click=on_click)
     key_listener = keyboard.Listener(on_press=on_press)
+
+
+    
